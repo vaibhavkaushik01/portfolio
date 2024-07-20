4e444d214dbf66d0e08d9708903a30ffcaf867d7
Vaibhav Kaushik <kaushikvaibhav563@gmail.com>
2024-07-21 03:35:13 +0530

    added responsiveness to website and a collapsing navbar

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-//when contact button in navbar get clicked instagram and whatsapp icon bounce for 2sec
-const contactbutton = document.querySelector(".contact-button");
+//when contact button in navbar get clicked instagram and whatsapp icon rotate for 2sec
+const contactbutton = document.querySelectorAll(".contact-button");
+console.log(contactbutton);
 
-contactbutton.addEventListener("click",()=>{
+contactbutton[0].addEventListener("click",()=>{
     const socialmediaimg = document.querySelectorAll(".social-media>a>img");
     socialmediaimg.forEach((icon)=>{
-        icon.classList.add("bounce");
+        icon.classList.add("rotate");
     });
     setTimeout(()=>{
         socialmediaimg.forEach((icon)=>{
-            icon.classList.remove("bounce");
+            icon.classList.remove("rotate");
         });
     },4000);
+});
+
+contactbutton[1].addEventListener("click",()=>{
+    const socialmediaimg = document.querySelectorAll(".social-media>a>img");
+    socialmediaimg.forEach((icon)=>{
+        icon.classList.add("rotate");
+    });
+    setTimeout(()=>{
+        socialmediaimg.forEach((icon)=>{
+            icon.classList.remove("rotate");
+        });
+    },4000);
+});
+
+
+// functionality for collapsed navbar
+document.getElementById("collapsed-navbar").addEventListener("click",()=>{
+    document.querySelector(".hidden-navbar").classList.add("hidden-navbar-translate-class");
+});
+
+
+//if any button in hidden navbar is clicked navbar hide again
+const hidden_nav_elem = document.querySelectorAll(".hidden-navbar-elem");
+hidden_nav_elem.forEach((elem)=>{   
+    elem.addEventListener("click",()=>{
+        document.querySelector(".hidden-navbar").classList.remove("hidden-navbar-translate-class");
+    });
 });
